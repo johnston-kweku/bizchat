c43143d26de01ec28660b7286dbe60fe6f4d78c1
Johnston Kweku <johnstonkweku@gmail.com>
2026-01-13 14:27:05 +0000

    Ignore .env file

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -147,15 +147,14 @@ class UpdateLastSeenMiddleware:
             UserProfile.objects.filter(user=request.user).update(last_seen=now())
         return self.get_response(request)
         
+import os
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'johnstonkweku@gmail.com'
-EMAIL_HOST_PASSWORD = 'ejup kqtk xfio ifxj'
-
-DEFAULT_FROM_EMAIL = 'Biz Chat <johnstonkweku@gmail.com>'
