2e1a67626019e0ec9b2802387efda98e443e86ee
Johnston Kweku <johnstonkweku@gmail.com>
2026-01-13 16:21:13 +0000

    Static

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -86,13 +86,14 @@ WSGI_APPLICATION = 'chatapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bizchat',
-        'USER': 'jr',
-        'PASSWORD': 'Johnston',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 
