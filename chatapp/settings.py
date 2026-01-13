fc91fe5a3d87289deb3c1ef43d9676f7127785e0
Johnston Kweku <johnstonkweku@gmail.com>
2026-01-13 16:40:08 +0000

    Static

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -83,16 +83,17 @@ WSGI_APPLICATION = 'chatapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  # Render requires SSL
+    )
 }
+
 
 
 
